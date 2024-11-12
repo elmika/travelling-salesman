f076ba3565631c62dc7f97b98b49c526c44f057e
Mikael Baron <mikael.baron@squaredpoint.com>
2024-11-12 11:01:13 +0100

    Clean test for counting iterations for n between 1 and 6.

--- src/test/java/PermutationsIteratorTest.java
+++ src/test/java/PermutationsIteratorTest.java
@@ -11,6 +11,16 @@ import com.elmika.tsp.JSONParsing;
 import com.elmika.tsp.PermutationsIterator;
 
 public class PermutationsIteratorTest {
+    
+    private int countIterationsForN(int n){
+        int i = 0;
+        PermutationsIterator iterator = new PermutationsIterator(n);
+        while(iterator.hasNext()) {
+            i++;
+            iterator.next();            
+        }
+        return i;
+    }
 
     private void printIterations(PermutationsIterator iterator){
         while(iterator.hasNext()) {
@@ -20,7 +30,17 @@ public class PermutationsIteratorTest {
     }
 
     @Test
-    public void testIterators() {
+    public void testCountIterations() {
+        assertEquals(1, countIterationsForN(1));
+        assertEquals(1, countIterationsForN(2));
+        assertEquals(2, countIterationsForN(3));
+        assertEquals(6, countIterationsForN(4));
+        assertEquals(24, countIterationsForN(5));
+        assertEquals(120, countIterationsForN(6));
+    }
+
+    // @Test
+    public void displayIterators() {
         
         for(int i=1; i<6; i++) {
             System.out.println("Running Iterator with value: "+i);
@@ -28,4 +48,10 @@ public class PermutationsIteratorTest {
             printIterations(iterator);
         }
     }
+
+
+    // Is correct if:
+    // 1) Has (n-1)! combinations
+    // 2) All combinations start with 1.
+    // 3) No two combinations are equal.
 }
