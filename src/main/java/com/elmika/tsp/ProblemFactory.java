847602f1d4d121afc8d902e53b24909f2b85508f
Mikael Baron <mikael.baron@squaredpoint.com>
2024-11-26 15:26:06 +0100

    Refactor problem types with size parameter.

--- src/main/java/com/elmika/tsp/ProblemFactory.java
+++ src/main/java/com/elmika/tsp/ProblemFactory.java
@@ -11,16 +11,32 @@ public class ProblemFactory {
         final String SIMPLE = "simple";
         final String BIGGER = "bigger";
         final String EUCLIDEAN = "euclidean";
+        final String CITIES = "cities";
 
-        switch(problemType){
+        switch(coreProblemType(problemType)){
             case TRIVIAL: problem = ProblemFactory.createSimplestProblem(); break;
             case SIMPLE: problem = ProblemFactory.createSimpleProblem(); break;
             case BIGGER: problem = ProblemFactory.createBiggerProblem(); break;
             case EUCLIDEAN: problem = ProblemFactory.createEuclideanProblem(); break;
+            case CITIES: problem = ProblemFactory.createEuclideanProblemOfSize(sizeOfProblemType(problemType)); break;            
             default: problem = ProblemFactory.createSimplestProblem(); break;
         }
 
         return problem;
+    }
+
+    private static String coreProblemType(String type) {
+        // Use a regular expression to remove trailing digits
+        return type.replaceAll("\\d+$", "");
+    }
+
+    private static int sizeOfProblemType(String type) {
+        // Use a regular expression to extract trailing digits
+        String trailingDigits = type.replaceAll(".*?(\\d+)$", "$1");
+
+        // Check if a number was found and return as an integer
+        return trailingDigits.matches("\\d+") ? Integer.parseInt(trailingDigits) : null;
+   
     }
 
     /*
@@ -79,6 +95,40 @@ public class ProblemFactory {
             {1.0, 7.0}
         };
         return new EuclideanProblem(points);
+    }
+
+    private static Problem createEuclideanProblemOfSize(int size) {
+        
+        if (size >15 || size <=0) {
+            throw new IllegalArgumentException("Cannot generate an Euclidean problem of size "+size+".");
+        }
+
+        double[][] fullArray = {
+            {37, 95},
+            {73, 59},
+            {15, 15},
+            {5, 86},
+            {60, 70},
+            {2, 96},
+            {83, 21},
+            {18, 18},
+            {30, 52},
+            {43, 29},
+            {61, 13},
+            {29, 36},
+            {45, 78},
+            {19, 51},
+            {59, 4}
+        };
+
+        // Truncate the array
+        double[][] truncated = new double[size][];
+        for (int j = 0; j < size; j++) {
+            truncated[j] = fullArray[j];
+        }
+
+        return new EuclideanProblem(truncated);
+        
     }
 
 }
