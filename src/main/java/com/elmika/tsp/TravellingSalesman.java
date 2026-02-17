4962bc11356ef0514280ce9303a2fb7cf7a9ce71
Mikael Baron <mika@squaredpoint.com>
2026-02-17 14:48:32 +0100

    Composition root in main

--- src/main/java/com/elmika/tsp/TravellingSalesman.java
+++ src/main/java/com/elmika/tsp/TravellingSalesman.java
@@ -1,33 +1,16 @@
 package com.elmika.tsp;
 
+/**
+ * Composition root: wires adapters and use case, then runs the CLI.
+ */
 public class TravellingSalesman {
 
     public static void main(String[] args) {
-        System.out.println("Traveling Salesman Problem Solver");
+        ConfigLoader configLoader = new JsonFileConfigLoader();
+        ProblemProvider problemProvider = new InMemoryProblemFactory();
+        TspSolver solver = new SolveTspUseCase();
 
-        // JSONParsing.test();
-        ProblemConfiguration config = JSONParsing.getConfig();
-        Problem problem = ProblemFactory.createProblem(config.getProblem());
-
-        SimpleSolver solver = new SimpleSolver(problem);
-        Solution solution = solver.findSolution(config.getResolutionStrategy());
-
-        displaySolution(solution);
+        TspCli cli = new TspCli(configLoader, problemProvider, solver);
+        cli.run();
     }
-
-    private static void displaySolution(Solution solution){
-        Integer[] sol = solution.getRoute();
-
-        StringBuilder solStringBuilder = new StringBuilder();
-        if (sol.length > 0) {
-            solStringBuilder.append(sol[0]);
-            for (int i = 1; i < sol.length; i++) {
-                solStringBuilder.append("->").append(sol[i]);
-            }
-        }
-
-        System.out.println("Solution is:" + solStringBuilder);
-        System.out.println("Distance is:" + solution.getTotalDistance());
-    }
-
 }
